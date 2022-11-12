3918ce74c9c202f90095f3aef913f527a3ce1184
ANSH54 <117513316+ANSH54@users.noreply.github.com>
2022-11-12 23:12:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+function MY(element) {
+         var currentColor = element.style.backgroundColor;
+         if(currentColor == "red") {
+            element.style.backgroundColor = "green";
+         } if(currentColor == "green") { element.style.backgroundColor
+           element.style.backgroundColor = "orange";
+  } if(currentColor == "orange") {
+    element.style.backgroundColor = "blue";
+  }if(currentColor == "blue") {
+    element.style.backgroundColor = "skyblue";
+  } if(currentColor == "skyblue") {
+    element.style.backgroundColor = "pink";
+  } if(currentColor == "pink") {
+    element.style.backgroundColor = "yellow";
+  } if(currentColor == "yellow") {
+    element.style.backgroundColor = "grey";
+  } if(currentColor == "grey") {
+    element.style.backgroundColor = "red";
+  }
+}
+function MY2() {
+ document.getElementById("owl").style.display = "block";
+}
+ function MY3() {
+ document.getElementById("owl").style.display = "none";
+ }
+function MY4() {
+  document.getElementById("heading").innerHTML = "THIS COLOUR BOX IS CREATED USING JAVASCRIPT AND HTML (JO TUMHARE SAMAJH KE BAHAR HAI ,, AB AGGE BADHO)";
+}
+function MY5() {
+  document.getElementById("heading").innerHTML = "COLOUR CHANGING BOX (CAN BE HIDDEN)"
+}
+function MY6() {
+  alert("AGYE NA AUKAT MEIN")
+  alert("RAHNE DO TUMSE NA HO PAYEGA")
+  alert("KARWA LI NA BEJJATI")
+  
+}
+function MY7() {
+  document.getElementById("image"). background = "1.jpg"
+}
+function MY8() {
+  document.getElementById("image"). background = "2.jpg"
+}
+function MY9() {
+  document.getElementById("image"). background = "3.jpg"
+}
+function MY10() {
+  document.getElementById("image"). background = "4.jpg"
+}
+function MY11() {
+  document.getElementById("image"). background = "5.jpg"
+}
+function MY12() {
+  document.getElementById("image"). background = "6.jpg"
+}
+function MY13() {
+  document.getElementById("image"). background = "7.jpg"
+}
+function MY14() {
+  document.getElementById("image"). background = "8.jpg"
+}
+function MY15() {
+  document.getElementById("arc").innerHTML="146";
+}
